44aedfd4c7d0bc50c8861eb5ecc50ed63e477787
nazeef_shaikh <nazeef_shaikh@GOD23777.persistent.co.in>
2016-01-22 14:10:14 +0530

    Minor changes-- prime number skeleton

--- ArithmeticOperations/src/com/persistent/ArithmeticOperations/serviceImpl/ArithmeticOperationsImpl.java
+++ ArithmeticOperations/src/com/persistent/ArithmeticOperations/serviceImpl/ArithmeticOperationsImpl.java
@@ -1,5 +1,5 @@
 package com.persistent.ArithmeticOperations.serviceImpl;
 
-public class ArithmeticOperationsImpl {
+public class ArithmeticOperationsImpl implements ArithmeticOperationsService{
 
 }
