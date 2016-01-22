61405de6caa09c1e39aefdcae1d922923516c791
nazeef_shaikh <nazeef_shaikh@GOD23777.persistent.co.in>
2016-01-22 15:21:39 +0530

    print in main

--- ArithmeticOperations/src/com/persistent/ArithmeticOperations/Application/ApplicationDemo.java
+++ ArithmeticOperations/src/com/persistent/ArithmeticOperations/Application/ApplicationDemo.java
@@ -8,6 +8,7 @@ public class ApplicationDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//////////
+		System.out.println("Nazeef print");
 	}
 
 }
--- ArithmeticOperations/src/com/persistent/ArithmeticOperations/serviceImpl/ArithmeticOperationsImpl.java
+++ ArithmeticOperations/src/com/persistent/ArithmeticOperations/serviceImpl/ArithmeticOperationsImpl.java
@@ -1,5 +1,12 @@
 package com.persistent.ArithmeticOperations.serviceImpl;
 
+import com.persistent.ArithmeticOperations.service.ArithmeticOperationsService;
+
 public class ArithmeticOperationsImpl implements ArithmeticOperationsService{
+
+	public void prineNumbers(int num) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
