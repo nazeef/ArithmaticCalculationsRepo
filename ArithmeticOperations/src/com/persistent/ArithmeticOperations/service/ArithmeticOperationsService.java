7b2e1950d1e30d561f4cf89dc5eac780299dec12
bindiya_gawas <bindiya_gawas@GOD23773.persistent.co.in>
2016-01-22 14:45:21 +0530

    commit 2:45

--- ArithmeticOperations/src/com/persistent/ArithmeticOperations/service/ArithmeticOperationsService.java
+++ ArithmeticOperations/src/com/persistent/ArithmeticOperations/service/ArithmeticOperationsService.java
@@ -3,5 +3,8 @@ package com.persistent.ArithmeticOperations.service;
 public interface ArithmeticOperationsService {
 	public void prineNumbers(int num);
 	public int factorial(int number);
+	
+	///addd
+	public int add(int num1,num2);
 	///asdfhaklbvf
 }
