fc00672a006e2dd01ab0924c02ba1d10ce2ac0fb
scheweta_naik <scheweta_naik@GOD23771.persistent.co.in>
2016-01-22 14:38:15 +0530

    cmts

--- ArithmeticOperations/src/com/persistent/ArithmeticOperations/service/ArithmeticOperationsService.java
+++ ArithmeticOperations/src/com/persistent/ArithmeticOperations/service/ArithmeticOperationsService.java
@@ -3,4 +3,5 @@ package com.persistent.ArithmeticOperations.service;
 public interface ArithmeticOperationsService {
 	public void prineNumbers(int num);
 	public int factorial(int number);
+	///asdfhaklbvf
 }
