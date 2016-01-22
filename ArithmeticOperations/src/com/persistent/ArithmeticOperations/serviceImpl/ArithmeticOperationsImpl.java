7b5a715400b2ace4c85467f8540b4697febe379c
scheweta_naik <scheweta_naik@GOD23771.persistent.co.in>
2016-01-22 15:17:11 +0530

    def fact fn

--- ArithmeticOperations/src/com/persistent/ArithmeticOperations/serviceImpl/ArithmeticOperationsImpl.java
+++ ArithmeticOperations/src/com/persistent/ArithmeticOperations/serviceImpl/ArithmeticOperationsImpl.java
@@ -2,4 +2,8 @@ package com.persistent.ArithmeticOperations.serviceImpl;
 
 public class ArithmeticOperationsImpl implements ArithmeticOperationsService{
 
+	public int factorial(int number){
+		System.out.println("scheweta");
+		return 1;
+	}
 }
