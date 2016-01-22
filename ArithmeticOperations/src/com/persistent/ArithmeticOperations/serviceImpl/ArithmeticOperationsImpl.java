71765c708421c2b8e942d883e88be28120d0ede7
scheweta_naik <scheweta_naik@GOD23771.persistent.co.in>
2016-01-22 15:18:47 +0530

    sch naik

--- ArithmeticOperations/src/com/persistent/ArithmeticOperations/serviceImpl/ArithmeticOperationsImpl.java
+++ ArithmeticOperations/src/com/persistent/ArithmeticOperations/serviceImpl/ArithmeticOperationsImpl.java
@@ -3,7 +3,7 @@ package com.persistent.ArithmeticOperations.serviceImpl;
 public class ArithmeticOperationsImpl implements ArithmeticOperationsService{
 
 	public int factorial(int number){
-		System.out.println("scheweta");
+		System.out.println("scheweta naik ");
 		return 1;
 	}
 }
