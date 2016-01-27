a352ea9590b500facb2ab1f62ed8da1af50ad0e0
vaibhav vaingankar <vbhvvaingankar9@gmail.com>
2016-01-27 15:43:00 +0530

    ultimate commit

--- ArithmeticOperations/src/com/persistent/ArithmeticOperations/Application/ApplicationDemo.java
+++ ArithmeticOperations/src/com/persistent/ArithmeticOperations/Application/ApplicationDemo.java
@@ -1,13 +1,9 @@
 package com.persistent.ArithmeticOperations.Application;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
-import com.persistent.ArithmeticOperations.exceptions.MyException;
 import com.persistent.ArithmeticOperations.serviceImpl.ArithmeticOperationsImpl;
 import com.persistent.ArithmeticOperations.util.FileReaderUtil;
 
@@ -15,110 +11,117 @@ public class ApplicationDemo {
 
 	/**
 	 * @param args
-	 * @throws IOException 
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 	
-		 BufferedReader in1 = new BufferedReader(new InputStreamReader(System.in));
-         ArithmeticOperationsImpl arithmeticOperations = new  ArithmeticOperationsImpl();
-		 int n=0,n1=0;
-		 
-		 
-		 //----------handling input-----------------------
-		 System.out.println("Enter a number to reverse: ");
-		 try {
-			n = Integer.parseInt(in1.readLine());
-		 } catch (Exception e) {}
-		 System.out.println("Reverse of " + n + " is " + arithmeticOperations.reverse(n));
-		 System.out.println("==========================================================================================");
-
-		 System.out.println("Enter a number to find if its palindrome: ");
-		 try {
-				n = Integer.parseInt(in1.readLine());
-			 } catch (Exception e) {}
-		 System.out.println(n + " is " + arithmeticOperations.palindrome(n));
-		 System.out.println("==========================================================================================");
-
-		 System.out.println("Enter a number to find the factorial: ");
-		 try {
-				n = Integer.parseInt(in1.readLine());
-			 } catch (Exception e) {}
-		 System.out.println("Factorial of " + n + " is " + arithmeticOperations.factorial(n));
-		 
-		 System.out.println("==========================================================================================");
-
-		 System.out.println("Enter a number to find if its armstrong: ");
-		 try {
-				n = Integer.parseInt(in1.readLine());
-			 } catch (Exception e) {}
-		 System.out.println(n + " is " + arithmeticOperations.armstrong(n));
-		 System.out.println("==========================================================================================");
-
-		 
-		 System.out.println("Enter the 2 numbers to perform the basic operations on them : ");
-		 try {
-				n = Integer.parseInt(in1.readLine());
-				n1 = Integer.parseInt(in1.readLine());
-			 } catch (Exception e) {}
-
-		 
-		 System.out.println("==========================================================================================");
-
-		 System.out.println("Addition : " + n + "+" + n1 + " = " + arithmeticOperations.add(n, n1));
-		 System.out.println("==========================================================================================");
-
-		 System.out.println("Product : " + n + "*" + n1 + " = " + arithmeticOperations.product(n,n1));
-
-		 
-		 try{
-			 basicOperations(n,n1);
-		 }catch(Throwable e){
-			 System.out.println("Caught Exception : "  +e);
-		 }
-
 		
-		 System.out.println("Printing prime nos , taking the input (range) from a file.");
-		 FileReaderUtil fileReader = new FileReaderUtil();
-		 String fileName = "C:\\Users\\scheweta_naik\\git\\ArithmaticCalculationsRepo\\ArithmeticOperations\\src\\demo.txt";
-		 
-		 arithmeticOperations.primeNumbers(Integer.parseInt(fileReader.readFile(fileName)));
-
-			
-		System.out.println("LCM of "+n+" and "+n1+" is "+arithmeticOperations.lcm(n,n1));
-
-		in1.close();
-			
-
-	}
-	
-	public static void basicOperations(int n,int n1) throws MyException{
+		 Scanner in = new Scanner(System.in);
 		 ArithmeticOperationsImpl arithmeticOperations = new  ArithmeticOperationsImpl();
-		
-		 System.out.println("Addition : " + n + "+" + n1 + " = " + arithmeticOperations.add(n, n1));
+		 
+		 int choice;
+		 do{
+			 
+		 System.out.println("Please Select Choice from below\n");
+		 System.out.println("1. Revese and check for palindrom number");
+		 System.out.println("2. Find Factorial of a number");
+		 System.out.println("3. Find LCM of two numbers");
+		 System.out.println("4. Perform some random operations on two numbers");
+		 System.out.println("5. Prime number (input from file)");
+		 System.out.println("6. Some more random operations on two numbers");
+		 System.out.println("7. Exit\n");
+							
+		 choice = in.nextInt();
+		 
+		 switch(choice){
+			 case 1:
+				 System.out.println("Enter a number to reverse: ");
+				 int n1 = in.nextInt();
+				 System.out.println("Reverse of " + n1 + " is " + arithmeticOperations.reverse(n1));
+				 System.out.println(n1 + " is " + arithmeticOperations.palindrome(n1));
+				break;
+				
+			 case 2:
+				 System.out.println("Enter a number to find the factorial: ");
+				 int n = in.nextInt();
+				 System.out.println("Factorial of " + n + " is " + arithmeticOperations.factorial(n));
+				 break;
+			 
+			 case 3:
+				 // lcm of two numbers
+					//Scanner sc = new Scanner(System.in);
+					
+					System.out.println("\n****Finding LCM of two numbers****\n\nEnter first number : ");
+					int a1 = in.nextInt();
+					System.out.println("\nEnter second number :  ");
+					int b1 = in.nextInt();
+					
+					System.out.println("LCM of "+a1+" and "+b1+" is "+arithmeticOperations.lcm(a1,b1));
+				
+					//sc.close();
+					break;
+			 case 4:
+				//calculate sum
+				 System.out.println("Enter the 2 numbers : ");
+				// Scanner inp = new Scanner(System.in);
+				 int num1 = in.nextInt();
+				 int num2=in.nextInt();
+				 
+				System.out.println("sum of " + num1 + "+" + num2 + " = " + arithmeticOperations.add(num1, num2));
+				System.out.println("remainder of " + num1 + "%" + num2 + " = " + arithmeticOperations.rem(num1, num2));
+				System.out.println("quotient of " + num1 + "/" + num2 + " = " + arithmeticOperations.quot(num1, num2));
+				 
+				 in.close();
+				 
+				 break;
+				 
+			 case 5:
+				 //-------------------File Input -------------------------------
+				 
+				 FileReaderUtil fileReader = new FileReaderUtil();
+				 String fileName = "src\\demo.txt";
+				 arithmeticOperations.primeNumbers(Integer.parseInt(fileReader.readFile(fileName)));
+				
+				 break;
+				 
+			 case 6:
+				 System.out.println("Enter the 2 numbers : ");
+					
+				 int num = in.nextInt();
+				 int number=in.nextInt();
 
-		 System.out.println("Product : " + n + "*" + n1 + " = " + arithmeticOperations.product(n,n1));
-		 
-		 System.out.println("Square root of " + n + " is " + arithmeticOperations.squareRoot(n)); 
-		 System.out.println("Square root of " + n1 + " is " + arithmeticOperations.squareRoot(n1));
-		 
-		 arithmeticOperations.OddEven(n);
-		 arithmeticOperations.OddEven(n1);
-		 
-		 System.out.println("LCM of "+n+" and "+n1+" is "+arithmeticOperations.lcm(n,n1));
-		 
-		 if(n1==0)
-			 throw new MyException("Division operations cannot be performed.Divide by zero");
-		 else{
-			 System.out.println("Division : " + n + "/" + n1 + " = " + arithmeticOperations.divide(n,n1));
-			 
-			 System.out.println("Remainder :  " + n + "%" + n1 + " = " + arithmeticOperations.rem(n, n1));
-			 
-			 System.out.println("Quotient : " + n + "/" + n1 + " = " + arithmeticOperations.quot(n, n1));
+				
+				 
+				 
+				
+				 //------------------------------------------------------
+				 arithmeticOperations.OddEven(number);
+				 
+				 System.out.println("product of " + num + "*" + number + " = " + arithmeticOperations.product(num,number));
+				 
+				 try{
+					 System.out.println("division of " + num + "/" + number + " = " + arithmeticOperations.divide(num,number));
+				 }
+				 catch(ArithmeticException ex){
+					 System.out.println("-------->   Caught Exception :"+ex.getMessage());
+				 }
+				 
+				 
+				 
+				 
+				 System.out.println("square root of " + number + " = " + arithmeticOperations.squareRoot(number));
+
+
+				 break;
+				 
 		 }
 		 
 		 
-
+		 
+		 }while(choice<7);
 		
+
+		// TODO Auto-generated method stub
+
 	}
 
 }
