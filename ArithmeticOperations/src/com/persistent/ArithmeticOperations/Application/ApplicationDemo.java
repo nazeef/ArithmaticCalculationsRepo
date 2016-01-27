4d6eee26379b22d32a21141c44b83554140ce9c7
priyanka_bandodkar <priyanka_bandodkar@GOD23776.persistent.co.in>
2016-01-27 15:13:24 +0530

    formating

--- ArithmeticOperations/src/com/persistent/ArithmeticOperations/Application/ApplicationDemo.java
+++ ArithmeticOperations/src/com/persistent/ArithmeticOperations/Application/ApplicationDemo.java
@@ -31,60 +31,76 @@ public class ApplicationDemo {
 		 System.out.println("Reverse of " + n + " is " + arithmeticOperations.reverse(n));
 		 
 		 
-		 
+		 System.out.println("==========================================================================================");
+
 		 System.out.println("Enter a number to find if its palindrome: ");
 		 try {
 				n = Integer.parseInt(in1.readLine());
 			 } catch (Exception e) {}
 		 System.out.println(n + " is " + arithmeticOperations.palindrome(n));
-		 
+		 System.out.println("==========================================================================================");
+
 		 System.out.println("Enter a number to find the factorial: ");
 		 try {
 				n = Integer.parseInt(in1.readLine());
 			 } catch (Exception e) {}
 		 System.out.println("Factorial of " + n + " is " + arithmeticOperations.factorial(n));
 		 
+		 System.out.println("==========================================================================================");
 
 		 System.out.println("Enter a number to find if its armstrong: ");
 		 try {
 				n = Integer.parseInt(in1.readLine());
 			 } catch (Exception e) {}
 		 System.out.println(n + " is " + arithmeticOperations.armstrong(n));
-		 
+		 System.out.println("==========================================================================================");
+
 		 
 		 System.out.println("Enter the 2 numbers to perform the basic operations on them : ");
 		 try {
 				n = Integer.parseInt(in1.readLine());
 				n1 = Integer.parseInt(in1.readLine());
 			 } catch (Exception e) {}
-		 
+		 System.out.println("==========================================================================================");
+
 		 System.out.println("Addition : " + n + "+" + n1 + " = " + arithmeticOperations.add(n, n1));
+		 System.out.println("==========================================================================================");
 
 		 System.out.println("Product : " + n + "*" + n1 + " = " + arithmeticOperations.product(n,n1));
 		 
 		 try{
+			 System.out.println("==========================================================================================");
+
 			 System.out.println("Division : " + n + "/" + n1 + " = " + arithmeticOperations.divide(n,n1));
-			 
+			 System.out.println("==========================================================================================");
+
 			 System.out.println("Remainder :  " + n + "%" + n1 + " = " + arithmeticOperations.rem(n, n1));
-			 
+			 System.out.println("==========================================================================================");
+
 			 System.out.println("Quotient : " + n + "/" + n1 + " = " + arithmeticOperations.quot(n, n1));
 		 }
 		 catch(ArithmeticException ex){
 			 System.out.println("Caught Exception : "+ex.getMessage());
 		 }
-		 
+		 System.out.println("==========================================================================================");
+
 		 System.out.println("Square root of " + n + " is " + arithmeticOperations.squareRoot(n)); 
 		 System.out.println("Square root of " + n1 + " is " + arithmeticOperations.squareRoot(n1));
 		 
 
 		 arithmeticOperations.OddEven(n);
+		 System.out.println("==========================================================================================");
+
 		 arithmeticOperations.OddEven(n1);
-		
+		 System.out.println("==========================================================================================");
+
 		 System.out.println("Printing prime nos , taking the input (range) from a file.");
 		 FileReaderUtil fileReader = new FileReaderUtil();
 		 String fileName = "C:\\Users\\scheweta_naik\\git\\ArithmaticCalculationsRepo\\ArithmeticOperations\\src\\demo.txt";
+		 
 		 arithmeticOperations.primeNumbers(Integer.parseInt(fileReader.readFile(fileName)));
-		
+		 System.out.println("\n==========================================================================================");
+
 		 System.out.println("\nLCM of 2 nos. : \n\nEnter first number : ");
 		 try {
 				n = Integer.parseInt(in1.readLine());
