c2fe9168a09ac10fbd1115a0341e38e646da3657
bindiya_gawas <bindiya_gawas@GOD23773.persistent.co.in>
2016-01-28 12:43:11 +0530

    commit o1

--- ArithmeticOperations/src/com/persistent/ArithmeticOperations/Application/ApplicationDemo.java
+++ ArithmeticOperations/src/com/persistent/ArithmeticOperations/Application/ApplicationDemo.java
@@ -27,12 +27,12 @@ public class ApplicationDemo {
 		 do{
 			 
 		 System.out.println("Please Select Choice from below\n");
-		 System.out.println("1. Revese and check for palindrom number");
+		 System.out.println("1. Revese and check for palindrom number, armstrong");
 		 System.out.println("2. Find Factorial of a number");
 		 System.out.println("3. Find LCM of two numbers");
 		 System.out.println("4. Perform some random operations on two numbers");
 		 System.out.println("5. Prime number (input from file)");
-		 System.out.println("6. Some more random operations on two numbers");
+		 System.out.println("6. Some more random operations on two numbers, fibonacci series");
 		 System.out.println("7. Exit\n");
 							
 		 try {
